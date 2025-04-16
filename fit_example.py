a0eb0e3a41b296d8528963d70ceb608132dcd185
unknown <iq000012@gmail.com>
2025-04-16 19:37:30 +0500

    data prep v0, reg fit

--- fit_example.py
+++ fit_example.py
@@ -1,27 +1,37 @@
+import pandas as pd
 from sklearn.datasets import load_breast_cancer
-from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.metrics import accuracy_score, roc_auc_score, mean_squared_error, r2_score
 from sklearn.model_selection import train_test_split
 
-from tabpfn import TabPFNClassifier
+from tabpfn import TabPFNRegressor
 
 
 def main():
-    # Load data
-    X, y = load_breast_cancer(return_X_y=True)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=42)
+    # Load data, tabpfn can't train (officially) on more than 10k examples
+    X_train = pd.read_pickle('pd_splits/x_train.pkl')[:10000]
+    X_test = pd.read_pickle('pd_splits/x_test.pkl')
+    y_train = pd.read_pickle('pd_splits/y_train.pkl')[:10000]
+    y_test = pd.read_pickle('pd_splits/y_test.pkl')
 
-    # Initialize a classifier
-    clf = TabPFNClassifier()
+    print(len(X_train), len(y_train), len(X_test), len(y_test))
 
-    clf.fit(X_train, y_train)
+    # Initialize the regressor
+    regressor = TabPFNRegressor()
+    print('Fit started')
+    regressor.fit(X_train, y_train)
+    print('Fit completed')
 
-    # Predict probabilities
-    prediction_probabilities = clf.predict_proba(X_test)
-    print("ROC AUC:", roc_auc_score(y_test, prediction_probabilities[:, 1]))
+    # Predict on the test set
+    print('Predictions started')
+    predictions = regressor.predict(X_test)
+    print('Predictions completed')
 
-    # Predict labels
-    predictions = clf.predict(X_test)
-    print("Accuracy", accuracy_score(y_test, predictions))
+    # Evaluate
+    mse = mean_squared_error(y_test, predictions)
+    r2 = r2_score(y_test, predictions)
+
+    print("Mean Squared Error (MSE):", mse)
+    print("R² Score:", r2)
 
 
 if __name__ == '__main__':
